4ef1001cb51b8e14a924cc12b8f92b5c97b8dc2a
Darshan Vadodariya <drvadodariya10@gmail.com>
2026-01-01 12:10:56 +0530

    urls changes

--- Demo5/urls.py
+++ Demo5/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import djando
 from django.contrib import admin
 from django.urls import path,include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({'status':'Live'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
